3b9dfb03cce10ca41c2aa81e6873fec747303acb
Jose Soto <jsoto7087@gmail.com>
2021-02-06 15:34:22 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+#!/usr/local/bin/ruby
+
+class Dog
+
+    def name=(dogs_name)
+        @this_name = dogs_name
+    end
+
+    def name 
+        @this_name
+    end 
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+
+class Person
+    def name=(person_name)
+        @this_person_name = person_name
+    end
+
+    def name 
+        @this_person_name
+    end
+
+end 
+    
+
+adele_goldberg = Person.new
+
+alan_kay = Person.new
+
+
+
