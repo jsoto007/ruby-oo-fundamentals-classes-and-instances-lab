f1cce9d5e2279177a829d4ceceec6874d2639c09
Jose Soto <jsoto7087@gmail.com>
2021-02-17 13:31:29 -0500

    commit tree b122dca0b95a032583dfd8db236468182b110079

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,46 @@
 #!/usr/local/bin/ruby
 
-class Dog
+# class Dog
 
-    def name=(dogs_name)
-        @this_name = dogs_name
-    end
+#     def name=(dogs_name)
+#         @this_name = dogs_name
+#     end
 
-    def name 
-        @this_name
-    end 
-end
+#     def name 
+#         @this_name
+#     end 
+# end
+
+# fido = Dog.new
+
+# snoopy = Dog.new
+
+# lassie = Dog.new
+
+
+
+class Dog 
+    attr_accessor :name 
+end 
+
 
 fido = Dog.new
 
 snoopy = Dog.new
 
 lassie = Dog.new
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,47 @@
 
 
-class Person
-    def name=(person_name)
-        @this_person_name = person_name
-    end
+# class Person
+#     def name=(person_name)
+#         @this_person_name = person_name
+#     end
 
-    def name 
-        @this_person_name
-    end
+#     def name 
+#         @this_person_name
+#     end
+
+# end 
+    
+
+# adele_goldberg = Person.new
+
+# alan_kay = Person.new
+
+
+
+
+class Person
+    attr_accessor :name
 
 end 
-    
 
 adele_goldberg = Person.new
 
 alan_kay = Person.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
